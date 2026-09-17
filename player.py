7fb31ed89579b9ea5860fc1fe0cf9ed0e0738631
senmid <inem.older@gmail.com>
2026-09-17 13:42:19 +0100

    feat: Users have full WASD movement

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
 import pygame
 
 
@@ -22,14 +22,16 @@ class Player(CircleShape):
     def rotate(self, dt: float) -> None:
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt: float) -> None:
+        self.position += pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SPEED * dt
+
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
-
-        # if keys[pygame.K_w]:
-        #     self.move(dt)
-        # if keys[pygame.K_s]:
-        #     self.move(-dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_a]:
-            self.rotate(dt)
-        if keys[pygame.K_d]:
             self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
