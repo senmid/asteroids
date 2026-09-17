d3e885a611262875193b6bded89e8f5b7905f492
senmid <inem.older@gmail.com>
2026-09-17 11:53:24 +0100

    feat: Users can now move the player left and right

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS
+from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED
 import pygame
 
 
@@ -18,3 +18,18 @@ class Player(CircleShape):
 
     def draw(self, screen) -> None:
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+
+    def rotate(self, dt: float) -> None:
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt: float) -> None:
+        keys = pygame.key.get_pressed()
+
+        # if keys[pygame.K_w]:
+        #     self.move(dt)
+        # if keys[pygame.K_s]:
+        #     self.move(-dt)
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
